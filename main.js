5addcb51fceff5b84d94d840de8184c7baa74301
Anna Sch <anna-schaeffner@gmx.de>
2018-10-17 17:22:52 +0200

    3

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hello")
+
+// Iteration 1
+
+function goToPage (link){
+  $('[data-page]').hide()
+
+   
+};
+
+goToPage('home');
+
+//Iteration 2: listen for click events on <a>
+//hint: event.preventDefault 
